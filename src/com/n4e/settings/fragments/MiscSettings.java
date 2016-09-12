834c69be9c29cd5b3ce1016bbf5f3add2819ba0e
Sang Tae Park <pastime1971@gmail.com>
2016-09-12 01:45:08 -0400

    Add option to disable scrolling cache [2/2]
    Change-Id: I5e6b06a2abd1238f720b55209a54619fc1e3cdd4
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- src/com/n4e/settings/fragments/MiscSettings.java
+++ src/com/n4e/settings/fragments/MiscSettings.java
@@ -13,11 +13,11 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
@@ -32,6 +32,11 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+    private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
+    private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
+    private static final String SCROLLINGCACHE_DEFAULT = "1";
+
+    private ListPreference mScrollingCachePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,11 +48,21 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         if (!DevelopmentSettings.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
             getPreferenceScreen().removePreference(findPreference(KEY_LOCK_CLOCK));
         }
+        // mScrollingCachePref
+        mScrollingCachePref = (ListPreference) findPreference(SCROLLINGCACHE_PREF);
+        mScrollingCachePref.setValue(SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP,
+                SystemProperties.get(SCROLLINGCACHE_PERSIST_PROP, SCROLLINGCACHE_DEFAULT)));
+        mScrollingCachePref.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mScrollingCachePref) {
+            if (newValue != null) {
+                SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, (String)newValue);
+            return true;
+            }
+         }
     return false;
     }
 
