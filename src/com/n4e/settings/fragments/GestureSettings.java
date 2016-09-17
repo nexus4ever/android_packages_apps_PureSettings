484469314bfc596eb9677971cc3f36150604d790
spezi77 <spezi77@gmx.com>
2016-09-17 22:44:02 +0200

    Align for NeXus4ever Project. Thanks to @beanstown106 and Nitrogen for the base.
    Signed-off-by: spezi77 <spezi77@gmx.com>

---
+++ src/com/n4e/settings/fragments/GestureSettings.java
@@ -0,0 +1,23 @@
+package com.n4e.settings.fragments;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class GestureSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.n4e_settings_gestures);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.NITROGEN_SETTINGS;
+    }
+}
